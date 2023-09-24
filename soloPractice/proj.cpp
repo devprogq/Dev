d27e8fbb8e868fd17f62fd0b2bdd567b8ea6f11d
Lautaro <devprog001@gmail.com>
2023-09-24 20:34:34 -0300

    second practice of cpp

--- soloPractice/proj.cpp
+++ soloPractice/proj.cpp
@@ -14,6 +14,7 @@ int main(){
 
 // Declarations in C++
 
+/*
 int main(){
 
     int x = 5; // Declaration for an integer (whole number)
@@ -31,3 +32,110 @@ int main(){
 
     return 0;
 }
+*/
+
+// Const variable
+
+/*
+int main(){
+    // The const keyword specifies that a variable's value is constant, tells the compiler to prevent anything from modifying it, so it's Read-only.
+
+    double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    std::cout << circumference << " cm" << '\n';
+
+    std::cout << "This is the speed of the light " << LIGHT_SPEED << '\n';
+
+    return 0;
+}
+*/
+
+// Namespaces
+
+/*
+namespace first {
+    int x = 1;
+}
+
+int main(){
+    // Provides a solution for preventing name conflicts in large projects. Each entity needs a unique name. This allows for identically named entities as long as the namespaces are different.
+
+    // We can use aswell the using namespace std::string and std::cout to not type always the std:: before doing a action
+
+    int x = 0;
+
+    // if we don't specify the prefix namespace, we use the value locally (in this case 0)
+
+    std::cout << x << '\n';
+
+    // first version of the namespace with the name of the variable. (in this case 1)
+
+    std::cout << first::x << '\n';
+
+    return 0;
+}
+*/
+
+// typedef
+
+/*
+#include <vector>
+
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
+
+int main(){
+    // reserved keyword to create and additional name for another data type, the new identifier for an existing type helps with readability and reduces typos.
+
+    text_t firstName = "Lautaro";
+    number_t number = 9;
+
+    std::cout << firstName << '\n';
+    std::cout << number << '\n';
+
+    return 0;
+}
+*/
+
+// arithmetic operators
+
+/*
+int main(){
+    // Return the result of a specific arithmetic operation (+ - * /)
+
+    // parenthesis first, multiplication & division second, lastly addition & subtraction
+
+    int students = 20;
+
+    // students = students + 1;
+    // students+=1;
+    // students++;
+
+    // students = students - 1;
+    // students-=1;
+    // students--;
+
+    // students = students * 2;
+    // students*=2;
+
+    // students = students / 2;
+    // students/=2;
+
+    // takes the remainder of the number 
+    int remainder = students % 2;
+
+    std::cout << students << '\n';
+    std::cout << remainder << '\n';
+
+    return 0;
+}
+*/
