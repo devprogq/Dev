cf1128fd2984ff2329059f61759200084f7dbefb
devprogq <devprog001@gmail.com>
2023-10-06 22:29:28 -0300

    first login test with input check

---
+++ soloPractice/cppProject/logIN.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string usernameStorage;
+    int passwordStorage;
+
+    string username;
+    int password;
+
+    cout << "Create your username: ";
+    cin >> usernameStorage;
+
+    cout << "Create your password: ";
+    cin >> passwordStorage;
+
+    cout << "Insert your username: ";
+    cin >> username;
+
+    cout << "Insert your password: ";
+    cin >> password;
+
+    if(password == passwordStorage && username == usernameStorage) {
+        cout << '\n' << "Logging in.";
+    } else {
+        cout << '\n' << "Try again.";
+    }
+
+    return 0;
+}
