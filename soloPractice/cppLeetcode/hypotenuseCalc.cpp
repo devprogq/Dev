b1e246f05c752106a6aac0109fdb80b6356fc4f7
devprogq <devprog001@gmail.com>
2023-10-09 12:38:17 -0300

    created a new cpp math program

---
+++ soloPractice/cppLeetcode/hypotenuseCalc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    double c;
+
+    cout << "First side value: ";
+    cin >> a;
+
+    cout << "Second side value: ";
+    cin >> b;
+
+    a = pow(a, 2);
+
+    b = pow(b, 2);
+
+    c = sqrt(a + b);
+
+    cout << "The result of the third side is: " << c;
+
+    return 0;
+}
