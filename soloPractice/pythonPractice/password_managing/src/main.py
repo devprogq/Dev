629c3af49877b079cd539498aa802a848ce29736
devprogq <devprog001@gmail.com>
2023-11-05 19:41:28 -0300

    i finished the program base, now i need to redifined some things to get a better feeling at it.

--- soloPractice/pythonPractice/password_managing/src/main.py
+++ soloPractice/pythonPractice/password_managing/src/main.py
@@ -1,19 +1,22 @@
+password = ""
+
 def createPassword():
+    global password
     password = input("Insert your password to start: ")
     print("This is your new password: " + password)    
 
 def deletePassword():
+    global password
     password = ""
     print("Now you don't have a password and have to create it again.")
-    createPassword()
 
-def changePassword():
+def changePassword(oldPassword):
     newPassword = input("Write your new password: ")
-    if (newPassword == password):
+    if newPassword == oldPassword:
         print("That is the same password you had before, try again.")
-        
     else:
-        print("This is the edited password: " + password)
+        print("This is your new password: " + newPassword)
+        return newPassword
 
 print("Hello to my password managing program, to start, save a first password")
 createPassword()
@@ -21,12 +24,12 @@ createPassword()
 print("Now that you have your first password, we can change it or delete it.")
 
 while True:
-    action = input("Press 1 to edit a password, press 2 to delete a password, press q to quit the program.")
+    action = input("Press 1 to edit a password, press 2 to delete a password, press q to quit the program: ")
 
     if action == 'q':
         break
 
-    elif (action == '1'):
-        changePassword()
-    elif (action == '2'):
+    elif action == '1':
+        password = changePassword(password)
+    elif action == '2':
         deletePassword()
